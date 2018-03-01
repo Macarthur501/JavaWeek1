a751157b071f16d4ad07f9526bb70a3aaadf3e54
Callum MacArthur <callum.macarthur501@googlemail.com>
2018-03-01 15:56:55 +0000

    Updating garage problem to add a runner class

---
+++ Garage example/RunnerClass.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class RunnerClass {
+	
+	public static void main(String[] args) {
+		
+		Garage garage1 = new Garage();
+		
+		
+		//System.out.println(calculateBill(findVehicle(vehiclesInGarage, "8998")));
+
+	}
+
+
+		
+
+		
+		
+	}
+
